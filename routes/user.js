2cace9f7077e5de06be64b5f8f533ff402c083c0
Om Urkude <omurkude274@gmail.com>
2025-06-16 13:15:23 +0530

    implemented user endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,90 @@
 const {Router}= require('express')
+const z = require('zod');
+const bcrypt = require('bcrypt');
+const jwt  = require('jsonwebtoken')
+const Jwtsecret =" userSecret" ;
 
 const UserRouter = Router();
 const { UserModel} = require('../db')
 
-UserRouter.post('/Login',function(req,res){
-    res.json({
-        msg :"hello"
-    })
+const signupbody = z.object({
+    email :z.string().email(),
+    password : z.string().min(6).max(20),
+    FirstName : z.string().min(3).max(50),
+    LastName : z.string().min().max(50)
 })
-UserRouter.post('/Signup',function(req,res){
-    res.json({
-        msg :"hello"
+
+const signinbody = z.object({
+    email :z.string().email(),
+    password :z.string().min(6).max(20)
+})
+
+
+UserRouter.post('/Signup', async function(req,res){
+    const data = signupbody.safeparse(req.body);
+    const salt  = 10 ;
+    if(!data.success){
+        return res.status(400).json({
+            msg : "Invalid Credentials",
+            errors : data.error.errors
+        
+        })
+    }
+    const {password} = data.data;
+    const hashedpass = bcrypt.hash(password,salt)
+    console.log(hashedpass)
+
+
+    await UserModel.create({
+        email,
+        hashedpass,
+        FirstName,
+        LastName
     })
+
+    res.status(200).json({
+        msg : "Account created / signed up"
+    })
+
+UserRouter.post('/Login', async function(req,res){
+    const response = signinbody.safeparse(req.body);
+
+    if(!response.success){
+        return res.status(400).json({
+            msg : "Invalid Credentials",
+
+        })
+    }
+    const {email ,password} = response.data;
+
+    const user = UserModel.findOne({
+        email
+    })
+
+    if (!user) {
+       return res.json({
+        msg : "User does not Exiists " 
+       })        
+    }
+
+    const passmatch = bcrypt.compare(password ,user.password)
+
+    if(passmatch){
+
+        const token = jwt.sign( { id: user._id.toString() },Jwtsecret);
+        return res.status(200).json({
+            msg: "LoggedIn",
+            token 
+        })
+
+    }else{
+        return res.status(400).json({
+            msg : "InvalidCredentials"
+        })
+    }
+    
+})
+
 })
 UserRouter.get('/Purchases',function(req,res){
     res.json({
